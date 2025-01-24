dd87753eb957b01ac986d6fc6b8f917ad6da2b29
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-01-24 12:34:38 +0100

    add leetcode practice

--- leetcode/array.py
+++ leetcode/array.py
@@ -2,7 +2,7 @@ import array
 
 class EasySolution(object):
     """
-    A class used to represent solution.
+    A class used to represent easy level solutions.
     """
     def duplicateZeros1089(self, arr: array) -> array:
         i=0
@@ -20,14 +20,56 @@ class EasySolution(object):
     # SC: O(1) We did inplace modification, no extra space.
 
    
+    def MergSortedArray88(self, num1: array, num2: array) -> array:
+        # compare A to B 
+        # if A>B => A put in C
+        # else => B put in C
+        # (pointer start from the last: ptr_a->num1 last / ptr_b->num2 last / ptr_c->last)
+        # pointer-=1
+
+        m = len(num1) - len(num2)
+        n = len(num2)
+  
+        ptr_a = m-1
+        ptr_b = n-1
+        last = m+n-1
+
+        while ptr_a>=0 and ptr_b>=0:
+            if num1[ptr_a] > num2[ptr_b]:
+                num1[last] = num1[ptr_a]
+                ptr_a-=1
+                last-=1
+            else:
+                num1[last] = num2[ptr_b]
+                ptr_b-=1
+                last-=1
+        
+        
+        while ptr_b>=0:
+            num1[last] = num2[ptr_b]
+            ptr_b-=1
+            last-=1
+
+        return num1  
+    # Input: num1=[1,2,3,0,0,0]
+    #        num2=[2,5,6]
+    # Output: [1,2,2,3,5,6]
+    # TC: O(n) => O(m)+O(n) => m is length of num1, n is length of num2
+    # SC: O(1) We did inplace modification, no extra space.
+
+
+
+
+   
 
 
 e = EasySolution()
-solution1 = e.duplicateZeros1089([1,0,2,3,0,4,5,0]) 
+sol1 = e.duplicateZeros1089([1,0,2,3,0,4,5,0]) 
+sol2 = e.MergSortedArray88([1,2,3,0,0,0],[2,5,6])
 
 
 
-
-print(solution1)
+print(sol1)
+print(sol2)
 
 

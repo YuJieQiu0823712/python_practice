47625dec288e1de85d36cfbec77041d1d9e1d278
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-02-14 17:01:53 +0100

    add linkedlist practice

--- py_algorithms/lists/merge_two_sorted.py
+++ py_algorithms/lists/merge_two_sorted.py
@@ -1,33 +1,3 @@
-class Node:
-    __slots__ = '_data', '_next'
-
-    def __init__(self, data, successor):
-        self._data = data
-        self._next = successor
-    
-    @property
-    def data(self):
-        return self._data
-    
-    @property
-    def next(self):
-        return self._next
-    
-    @next.setter
-    def next(self, other: 'Node'): # 'Node' is used as a forward reference to avoid issues with the class not being fully defined yet.
-        if other is None:
-            self._next = None
-            return
-        
-        if not isinstance(other, self.__class__):
-            raise ValueError("Type invariant violation.")
-        
-        self._next = other
-    
-    def __repr__(self):
-        return '#<{} data={} next={}>' \
-        .format(self.__class__.__name__, self.data, self.next)
-
 class MergeTwoSorted:
     @staticmethod
     def apply(a:Node, b:Node) -> Node:

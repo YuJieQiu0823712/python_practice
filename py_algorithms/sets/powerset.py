450f1aad06707b2f78ca448c2c689ca8e4e569b3
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-02-16 17:32:11 +0100

    add powerset_test.py

--- py_algorithms/sets/powerset.py
+++ py_algorithms/sets/powerset.py
@@ -1,13 +1,14 @@
+from typing import Any
+from typing import List
 
-
-class Powerset:
+class PowerSet:
     @staticmethod
     def apply(xs: List[Any]) -> List[Any]: # Any => can contain elements of any data type
         """Takes a list xs as input and returns a list of all possible subsets."""
         result = []
 
         # A bitwise left shift operation equivalent to 2^len(xs).
-        cadinality = 1 << len(xs)
+        cardinality = 1 << len(xs)
         # This calculates the total number of subsets.
         # If xs = [1, 2, 3] => cardinality = 2^3 = 8 (decimal) subsets in a powerset
         #                   => cardinality = 1 << 3 => shift left 3 times => 1000 (binary)
@@ -23,5 +24,26 @@ class Powerset:
             result.append(stack)
             i += 1
         return result
+
+        ## i is the row number (from 0 to 7 in decimal)
+        # Each bit in i determines whether we include xs[j] in the subset
+        # j is the position of each bit in i, mapping to elements in xs.
+
+        # Ex:
+        # i = 0b011  (3 in decimal) 
+        # j = 0       (checking bit position 0)  
+        # -> 0b011 & (1 << 0)  → 0b011 & 0b001  → 1  → include xs[0] (a)
+        
+        # j = 1       (checking bit position 1)  
+        # -> 0b011 & (1 << 1)  → 0b011 & 0b010  → 1  → include xs[1] (b)
+
+        # j = 2       (checking bit position 2)  
+        # -> 0b011 & (1 << 2)  → 0b011 & 0b100  → 0  → do not include xs[2] (c)
+
+        # => for i = 3 (0b011), the subset is [a, b].
+
+        #  Expression	  Binary Calculation 	Decimal Result
+        # 0b011 & 0b001	   011 & 001 = 001	      1
+        # 0b011 & 0b100	   011 & 100 = 000	      0
 
                     

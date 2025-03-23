e5949c2d0ebca36723fd979dea916daa732ea4c3
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-03-23 23:45:55 +0100

    add maximumSubarray53

--- leetcode/Array/arrayMedium.py
+++ leetcode/Array/arrayMedium.py
@@ -42,10 +42,32 @@ class MediumSolution(object):
     # Total profit is 4 + 3 = 7.
     # TC: O(n)
     # SC: O(1)
+
+
+    def maximumSubarray53(self,nums: array) -> int:
+        # Kadane's Algorithm:
+        # Tracks the current subarray sum. Start with the first element (nums[0]).
+        # Tracks the maximum sum encountered. Start with the first element (nums[0]).
+        # Iterate through the array (extend the current subarray or start a new subarray)
+        # Return the maximum subarray sum found (total_max).
+        total_sum = nums[0]
+        total_max = nums[0]
+        for i in range(1,len(nums)):
+            total_sum = max(total_sum + nums[i],nums[i])
+            total_max = max(total_max,total_sum)
+        return total_max    
+    # Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    # Output: 6
+    # Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+    # TC: O(n)
+    # SC: O(1)
+
     
 m = MediumSolution()
 sol1 = m.mergeIntervals56([[1,3],[2,6],[8,10],[15,18]])
 sol2 = m.bestTimeToBuyAndSellStockII122([7,1,5,3,6,4])
+sol3 = m.maximumSubarray53([-2,1,-3,4,-1,2,1,-5,4])
 
 print(sol1)
 print(sol2)
+print(sol3)

946073998e42a491677f7621f85495f57f9be3cb
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-03-30 22:35:37 +0200

    add SparseVector1570

--- leetcode/HashMap/hashMapMedium.py
+++ leetcode/HashMap/hashMapMedium.py
@@ -65,3 +65,34 @@ print(wordDictionary.search("pad")) #return False
 print(wordDictionary.search("bad")) #return True
 print(wordDictionary.search(".ad")) #return True
 print(wordDictionary.search("b..")) #return True
+
+
+class SparseVector1570:
+    def __init__(self,nums: list[int]):
+        # store only non-zero value using dictionary {index: value}
+        # perform the dot product only on common indices
+        self.non_zero = {}
+        for idx, val in enumerate(nums):
+            if val != 0:
+                self.non_zero[idx] = val
+    
+    def dotProduct(self, vec: 'SparseVector1570') -> int:
+        result = 0
+        for key, val in (self.non_zero.items()):
+            if key in vec.non_zero:
+                result += val * vec.non_zero[key] 
+        return result
+    # Implement class SparseVector:
+    # SparseVector(nums) Initializes the object with the vector nums
+    # dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+    # A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+    # Follow up: What if only one of the vectors is sparse?
+    
+    # Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+    # Output: 8
+    # Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+    # v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+
+vec1 = SparseVector1570([1,0,0,2,3])
+vec2 = SparseVector1570([0,3,0,4,0])
+print(vec1.dotProduct(vec2)) 

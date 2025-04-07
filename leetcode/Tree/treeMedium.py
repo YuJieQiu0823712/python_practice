a28386519e0a96e578d47ef9a927abcd35528f13
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-04-07 11:34:43 +0200

    add ValidateBinarySearchTree98

--- leetcode/Tree/treeMedium.py
+++ leetcode/Tree/treeMedium.py
@@ -37,6 +37,47 @@ class mediumSolution:
     # SC: O(n) 
 
 
+    # 1. Iterative
+    def ValidateBinarySearchTree98(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        
+        stack = [(root, float('-inf'), float('inf'))]
+        while stack:
+            root, lowest, highest = stack.pop()
+            if not root:
+                continue
+            if root.val <= lowest or root.val >= highest:
+                return False
+            stack.append((root.left, lowest, root.val))
+            stack.append((root.right, root.val, hightst))
+        return True
+        # TC: O(n) 
+        # SC: O(n) stack
+        
+    # 2. Recursive
+    def isValidBST(self, root: TreeNode) -> bool:
+        return self.is_valid(root, float('-inf'), float('inf'))
+        
+    def is_valid(self, root, lowest, highest):
+        if not root: 
+            return True
+        if root.val <= lowest or root.val >= highest:
+            return False
+        return self.is_valid(root.left, lowest, root.val) and self.is_valid(root.right, root.val, highest)
+    # TC: O(n) 
+    # SC: O(n) call stack
+
+    # Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+    # A valid BST is defined as follows:
+    # The left subtree of a node contains only nodes with keys less than the node's key.
+    # The right subtree of a node contains only nodes with keys greater than the node's key.
+    # Both the left and right subtrees must also be binary search trees.
+    
+    # Input: root = [5,1,4,null,null,3,6]
+    # Output: false
+    # Explanation: The root node's value is 5 but its right child's value is 4.
+
 
 m = mediumSolution()
 

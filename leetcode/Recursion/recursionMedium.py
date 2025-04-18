ce907276f33822410a709256ad2c73a084ca8b76
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-04-18 11:05:39 +0200

    add maxAreaOfIsland695

--- leetcode/Recursion/recursionMedium.py
+++ leetcode/Recursion/recursionMedium.py
@@ -26,7 +26,7 @@ class MediumSolution(object):
     # SC: O(n!) - space for the result list (curr_list shallow copy), n is the length of nums
 
 
-    def combinationSum(self, candidate: list[int], target: int) -> list[list[int]]:
+    def combinationSum39(self, candidate: list[int], target: int) -> list[list[int]]:
         self.res = []
         def backtracking(candicate, temp_list, remainder, index):
             # base case
@@ -50,9 +50,60 @@ class MediumSolution(object):
     # 7 is a candidate, and 7 = 7.
     # These are the only two combinations. 
 
+    def maxAreaOfIsland695(self, grid: list[list[int]]) -> int:
+        if not grid:
+            return 0
+        size = 0
+        max_island = 0
+        row_len = len(grid)
+        col_len = len(grid[0])
+        for row in range(row_len):
+            for col in range(col_len):
+                if grid[row][col] == 1:
+                    island_size = self.count_island_size(grid, row, col, 1)
+                    max_island = max(max_island, island_size)
+        return max_island
+    def count_island_size(self, grid, row, col, size):
+        if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] != 1:
+            return 0
+        grid[row][col] = 0 # mark as visited
+        directions = [(row-1, col), (row+1, col), (row, col-1),(row, col+1)]
+        for r,c in directions:
+            size += self.count_island_size(grid, r, c, 1)
+        return size
+
+    # You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
+    # You may assume all four edges of the grid are surrounded by water.
+    # The area of an island is the number of cells with a value 1 in the island.
+    # Return the maximum area of an island in grid. If there is no island, return 0.
+
+    # Input: grid = 
+    # [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+    # [0,0,0,0,0,0,0,1,1,1,0,0,0],
+    # [0,1,1,0,1,0,0,0,0,0,0,0,0],
+    # [0,1,0,0,1,1,0,0,1,0,1,0,0],
+    # [0,1,0,0,1,1,0,0,1,1,1,0,0],
+    # [0,0,0,0,0,0,0,0,0,0,1,0,0],
+    # [0,0,0,0,0,0,0,1,1,1,0,0,0],
+    # [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    # Output: 6
+    # Explanation: The answer is not 11, because the island must be connected 4-directionally.
+    
+    # TC: O(R*C) - R is the number of rows, C is the number of columns
+    # SC: O(R*C) - space for the grid (visited) and recursion stack
+
 m = MediumSolution()
 sol1 = m.Permutations46([1,2,3])
-sol2 = m.combinationSum([2,3,6,7], 7)
+sol2 = m.combinationSum39([2,3,6,7], 7)
+sol3 = m.maxAreaOfIsland695([[0,0,1,0,0,0,0,1,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+                            [0,1,1,0,1,0,0,0,0,0,0,0,0],
+                            [0,1,0,0,1,1,0,0,1,0,1,0,0],
+                            [0,1,0,0,1,1,0,0,1,1,1,0,0],
+                            [0,0,0,0,0,0,0,0,0,0,1,0,0],
+                            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+                            [0,0,0,0,0,0,0,1,1,0,0,0,0]])   
 
 print(sol1)
 print(sol2)
+print(sol3)

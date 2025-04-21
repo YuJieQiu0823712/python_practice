36ec95256142e723730815a866a22c8b7cd72882
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-04-21 12:11:34 +0200

    add addStrings415

--- leetcode/Strings/stringsEasy.py
+++ leetcode/Strings/stringsEasy.py
@@ -34,6 +34,51 @@ class EasySolutions:
     # SC: O(n) - n is the length of the stack
 
 
+    def addStrings415(self, num1: str, num2: str) -> str:
+        num1_ptr = len(num1) - 1
+        num2_ptr = len(num2) - 1
+        res = []
+        carry = 0
+        while num1_ptr >= 0 or num2_ptr >= 0 or carry > 0:
+            if num1_ptr >= 0:
+                carry += (ord(num1[num1_ptr]) - ord('0'))
+                num1_ptr -= 1
+            if num2_ptr >= 0:
+                carry += (ord(num2[num2_ptr]) - ord('0'))
+                num2_ptr -= 1
+            res += str(carry % 10) 
+            # if you use res='' and return result[::-1]
+            # => TC: O(n) - because string is immutable
+            # total TC: O(n^2) - n is the length of the longest string
+            carry //= 10
+
+        # 1
+        return ''.join(result[::-1])
+        # TC: O(max(num1,num2)) - n is the length of the longest string
+        # SC: O(1)
+
+        # 2
+        # revert = []
+        # for char in res:
+        #     revert.append(char)
+        # return ''.join(revert)
+        # TC: O(max(num1,num2)) 
+        # SC: O(max(num1,num2)) - n is the length of the longest string
+
+        # 3
+        # re_length = len(res)
+        # res2 = ''
+        # for i in range(res_length):
+        #     res2 += res[res_length -1 - i]
+        # return res2
+        # TC: O(max(num1,num2))
+        # SC: O(max(num1,num2))
+    # Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+    # You must solve the problem without using any built-in library for handling large integers (such as BigInteger). 
+    # You must also not convert the inputs to integers directly.
+    # Input: num1 = "11", num2 = "123"
+    # Output: "134"
+
 
 e = EasySolutions()
 sol1 = e.validParentheses20("()[]{}")

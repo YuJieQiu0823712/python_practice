e58ce3c08036918d0f9f7f2804bba37d2f560fa6
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-05-12 22:38:26 +0200

    add twoKeysKeyboard650

--- leetcode/Dynamic_programming/dynamicProgrammingMedium.py
+++ leetcode/Dynamic_programming/dynamicProgrammingMedium.py
@@ -8,3 +8,15 @@ class MediumSolution:
                     if s[i:i+len(word)] == word:
                         dp[i+len(word)] = True
         return dp[-1]
+
+    def twoKeysKeyboard650(self, n: int) -> int:
+        dp = [i for i in range(n+1)]
+        dp[1] = 0
+
+        for curr_n in range(2,n+1):
+            for curr_chars_can_be_copied_and_pasted in range(curr_n//2, 1, -1):
+                if curr_n % curr_chars_can_be_copied_and_pasted == 0:
+                    dp[curr_n] = dp[curr_chars_can_be_copied_and_pasted] + curr_n//curr_chars_can_be_copied_and_pasted
+                    break
+        return dp[n]
+
--- leetcode/Dynamic_programming/dynamicProgrammingMedium_test.py
+++ leetcode/Dynamic_programming/dynamicProgrammingMedium_test.py
@@ -4,3 +4,8 @@ def test_wordBreak139():
     m = MediumSolution()
     assert m.wordBreak139("leetcode", ["leet","code"]) == True
     assert m.wordBreak139("catsandog", ["cats","dog","sand","and","cat"]) == False
+
+def test_twoKeysKeyboard650():
+    m = MediumSolution()
+    assert m.twoKeysKeyboard650(3) == 3
+    assert m.twoKeysKeyboard650(9) == 6

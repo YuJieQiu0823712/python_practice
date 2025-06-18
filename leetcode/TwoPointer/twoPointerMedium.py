587d953f18b86b63af898487fb7514cb2cfe07b9
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-06-18 23:34:54 +0200

    add three sum smaller 259

--- leetcode/TwoPointer/twoPointerMedium.py
+++ leetcode/TwoPointer/twoPointerMedium.py
@@ -108,3 +108,19 @@ class MediumSolution:
                 else:
                     right -= 1
         return target - diff
+
+
+    def threeSumSmaller259(self, nums: list[int], target: int) -> int:
+        res = 0
+        nums.sort()
+        for i in range(len(nums) -2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                if nums[left] + nums[right] < target - nums[i]:
+                    res += right - left
+                    left += 1
+                else:
+                    right -= 1
+        return res

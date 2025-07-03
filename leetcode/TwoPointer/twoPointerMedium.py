9b66540ba867a7c6375a08659e7bb04e124e1102
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-07-03 22:51:52 +0200

    add fourSum18

--- leetcode/TwoPointer/twoPointerMedium.py
+++ leetcode/TwoPointer/twoPointerMedium.py
@@ -195,3 +195,51 @@ class MediumSolution:
             else:
                 i += 1
         return nums
+
+    def fourSum18(self, nums: list[int], target: int) -> list[list[int]]:
+        """
+        Finds all unique quadruplets in the array that sum up to a specific target.
+
+        Args:
+            nums (List[int]): A list of integers.
+            target (int): The target sum to find.
+
+        Returns:
+            List[List[int]]: A list of lists, where each inner list contains four integers
+                             that sum to the target. The result does not contain duplicate quadruplets.
+
+        TC: O(n^3)
+        SC: O(n) 
+        """
+        nums.sort()
+        res = []
+        length = len(nums)
+
+        # edge check
+        if length < 4:
+            return []
+
+        for i in range(length - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, length - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = length - 1
+                
+                while left < right:
+                    curr_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    if curr_sum < target:
+                        left += 1
+                    elif curr_sum > target:
+                        right -= 1
+                    else:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+        return res

4e693b2ca51280d9812338bdb23acfcdd8460967
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-07-20 20:18:51 +0200

    taskScheduler621

--- leetcode/Heap/heapMedium.py
+++ leetcode/Heap/heapMedium.py
@@ -1,4 +1,5 @@
 import heapq
+import collections
 
 class MediumSolution:
     def kthLargestElementInAnArray215(self, nums: list[int], k: int) -> int:
@@ -18,7 +19,50 @@ class MediumSolution:
         min_heap = []
         for num in nums:
             if len(min_heap) < k:
-                heapq.heappush(min_heap, num)
+                heapq.heappush(min_heap, num) # sorted order in min-heap
             else:
                 heapq.heappushpop(min_heap, num)
         return min_heap[0]
+
+    
+    def taskScheduler621(self, tasks: list[str], n: int) -> int:
+        """
+        Calculates the least number of intervals needed to finish all tasks with cooldown.
+
+        Each same task must be executed at least `n` intervals apart. The CPU can remain idle
+        if there are no eligible tasks to run during an interval.
+
+        Args:
+            tasks (list[str]): A list of tasks represented by capital letters.
+            n (int): The non-negative cooling interval between same tasks.
+
+        Returns:
+            int: The minimum number of intervals required to finish all tasks.
+
+        TC: O(n log k), where n is the number of tasks and k is the number of unique tasks.
+        SC: O(k), where k is the number of unique tasks.   
+        """
+    
+        tasks_count = collections.Counter(tasks).values()
+        max_heap = []
+        count = 0
+
+        for value in tasks_count:
+            heapq.heappush(max_heap, -value)  # Use negative values for max-heap behavior
+        
+        while max_heap:
+            remain_tasks = []
+
+            for _ in range(n+1):
+                count += 1
+                
+                if max_heap:
+                    curr_tasks_value = - heapq.heappop(max_heap)
+                    if curr_tasks_value - 1 > 0:
+                        remain_tasks.append(curr_tasks_value - 1)
+                if not max_heap and not remain_tasks:
+                    return count
+            
+            for task in remain_tasks:
+                heapq.heappush(max_heap, -task)
+
--- leetcode/Heap/test_heapMedium.py
+++ leetcode/Heap/test_heapMedium.py
@@ -10,3 +10,14 @@ def test_kthLargestElementInAnArray215(nums, k, expected):
     m = MediumSolution()
     result = m.kthLargestElementInAnArray215(nums, k)
     assert result == expected
+
+
+@pytest.mark.parametrize("tasks, n, expected", [
+    (["A","A","A","B","B","B"], 2, 8), # A -> B -> idle -> A -> B -> idle -> A -> B
+    (["A","A","A","B","B","B"], 0, 6), # A -> A -> A -> B -> B -> B
+])
+
+def test_taskScheduler621(tasks, n, expected):
+    m = MediumSolution()
+    result = m.taskScheduler621(tasks, n)
+    assert result == expected

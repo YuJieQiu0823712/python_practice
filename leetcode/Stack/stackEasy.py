3a6c0465ad977946e234e8478507d139508f7628
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-07-29 23:08:11 +0200

    add easy stack RemoveAllAdjacentDuplicatesInString1047

---
+++ leetcode/Stack/stackEasy.py
@@ -0,0 +1,10 @@
+class EasySolution:
+    def RemoveAllAdjacentDuplicatesInString1047(self,s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
+

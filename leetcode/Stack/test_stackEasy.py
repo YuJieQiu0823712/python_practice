fb6bc8ec8fce3173bd707a3f93104b204c57a11f
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-07-30 23:00:12 +0200

    add test RemoveAllAdjacentDuplicatesInString1047

--- leetcode/Stack/test_stackEasy.py
+++ leetcode/Stack/test_stackEasy.py
@@ -1,1 +1,12 @@
 import pytest
+from stackEasy import EasySolution
+
+@pytest.mark.parametrize("input_str,expected", [
+    ("abbaca", "ca"),
+    ("azxxzy", "ay")
+])
+
+def test_RemoveAllAdjacentDuplicatesInString1047(input_str, expected):
+    e = EasySolution()
+    result = e.RemoveAllAdjacentDuplicatesInString1047(input_str)
+    assert result == expected

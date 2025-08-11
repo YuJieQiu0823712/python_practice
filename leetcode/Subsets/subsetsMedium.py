df8db114486bcf692e2f7767b34ab567291beb5c
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-08-11 22:33:13 +0200

    add subsetsWithDup90

--- leetcode/Subsets/subsetsMedium.py
+++ leetcode/Subsets/subsetsMedium.py
@@ -12,3 +12,24 @@ class mediumSolution:
                 copy_set.append(num)
                 subset.append(copy_set)
         return subset
+
+    def subsetsWithDup90(self, nums: List[int]) -> List[List[int]]:
+       nums.sort()
+       subsets = []
+       subsets.append([])
+       start_index = 0
+       end_index = 0
+
+       for i in range(len(nums)):
+        start_index = 0
+
+        for i in ramge(len(nums)):
+            start_index = 0
+            if i > 0 and nums[i] == nums[i - 1]:
+                start_index = end_index + 1
+            end_index = len(subsets) - 1
+            for j in range(start_index, end_index + 1):
+                new_subset = list(subsets[j])
+                new_subset.append(nums[i])
+                subsets.append(new_subset)
+        return subsets

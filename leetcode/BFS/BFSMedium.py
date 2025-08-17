95def5c8e18bb5813dbab15a9eb57bb4395549c4
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-08-17 21:53:56 +0200

    add BFS

---
+++ leetcode/BFS/BFSMedium.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class mediumSolution:
+    def levelOrder(self, root: TreeNode) -> list[list[int]]:
+        """
+        TC: O(n), n is the number of nodes in the tree
+        SC: O(n)
+        """
+        if not root:
+            return []
+
+        res = []
+        queue = collection.deque()
+        queue.append(root)
+
+        while queue:
+            queue_len = len(queue)
+            curr_list = []
+
+            for _ in range(queue_len):
+                node = queue.popleft()
+                curr_list.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(curr_list)
+
+        return res

93b3c804c89b2f7519cc248447d64f611fd62945
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-08-27 12:19:34 +0200

    add minDepth111

--- leetcode/BFS/BFSEasy.py
+++ leetcode/BFS/BFSEasy.py
@@ -35,4 +35,29 @@ class EasySolution:
             res.append(curr_sum / queue_len)
         
         return res
+
+    def minDepth111(self, root: Optional[TreeNode]) -> int:
+        """
+        TC: O(n), n is the number of nodes in the tree
+        SC: O(n)
+        """
+        if not root:
+            return 0
+
+        level = 0
+        queue = deque()
+        queue.append(root)
+        while queue:
+            level += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                if not node.left and not node.right:
+                    return level
+                else:
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+        return level
   

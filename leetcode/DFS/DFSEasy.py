aba61f8791177c863378f7229ea586bff35da97b
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-09-07 23:02:33 +0200

    add pathSum113

--- leetcode/DFS/DFSEasy.py
+++ leetcode/DFS/DFSEasy.py
@@ -19,3 +19,29 @@ class EasySolution:
             return True
 
         return self.hasPathSum112(root.left, targetSum - root.val) or self.hasPathSum112(root.right, targetSum - root.val)
+
+    def pathSum113(self, root: TreeNode, targetSum: int) -> list[list[int]]:
+        """
+        TC: O(n^2)
+        SC: O(n)
+        """
+        def find_path(curr_node, target_sum, curr_path, all_paths):
+            if not curr_node:
+                return
+
+            curr_path.append(curr_node.val)
+
+            # Base case
+            if not curr_node.left and not curr_node.right and curr_node.val == target_sum:
+                all_paths.append(list(curr_path))
+            else:
+                target_sum -= curr_node.val
+                find_path(curr_node.left, target_sum, curr_path, all_paths)
+                find_path(curr_node.right, target_sum , curr_path, all_paths)
+
+            # Backtrack
+            curr_path.pop()
+        all_paths = []
+        find_path(root, targetSum, [], all_paths)
+        return all_paths
+    

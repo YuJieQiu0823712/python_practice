f1209136d3cb6768811d00d5f827876e31155fe5
Yu-Jie <chiou.jade@gmail.com>
2025-09-25 22:19:50 +0200

    add test_middleOfTheLinkedList876

--- leetcode/FastAndSlowPointers/test_fastAndSlowPointerEasy.py
+++ leetcode/FastAndSlowPointers/test_fastAndSlowPointerEasy.py
@@ -12,7 +12,20 @@ def create_linked_list(values, pos):
         nodes[-1].next = nodes[pos]
     return nodes[0]
 
+def build_linked_list(values):
+    dummy = ListNode(0)
+    curr = dummy
+    for v in values:
+        curr.next = ListNode(v)
+        curr = curr.next
+    return dummy.next
 
+def linked_list_to_list(node: ListNode):
+    res = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    return res
 
 @pytest.mark.parametrize("values, pos, expected", [
     ([3,2,0,-4], 1,True),
@@ -27,3 +40,14 @@ def test_linkedListCycle141(values, pos, expected):
     assert result == expected
 
 
+@pytest.mark.parametrize("head_list, expected", [
+        ([1, 2, 3, 4, 5], [3, 4, 5]),
+        ([1, 2, 3, 4, 5, 6], [4, 5, 6]),
+        ([1], [1]),
+        ([1, 2], [2]),
+])
+def test_middleOfTheLinkedList876(head_list, expected):
+    head = build_linked_list(head_list)
+    e = EasySolution()
+    result = e.middleOfTheLinkedList876(head)
+    assert linked_list_to_list(result) == expected
